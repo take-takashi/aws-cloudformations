33882957cb90ec7725956298a3cceb8e82ea5821
TAKE Takashi <puller.swipes1h@icloud.com>
2025-09-30 23:58:44 +0900

    最低限のcognitoログインを定義

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,29 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { Amplify } from 'aws-amplify'
+import type { ResourcesConfig } from 'aws-amplify'
+
+const amplifyConfig: ResourcesConfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: import.meta.env.VITE_COG_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_COG_APP_CLIENT_ID,
+      loginWith: {
+        oauth: {
+          domain: import.meta.env.VITE_COG_DOMAIN,
+          scopes: ['openid', 'email', 'profile'],
+          redirectSignIn: [import.meta.env.VITE_REDIRECT],
+          redirectSignOut: [import.meta.env.VITE_REDIRECT],
+          responseType: 'code',
+        },
+      },
+    },
+  },
+};
+
+Amplify.configure(amplifyConfig);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
